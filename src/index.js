d7893dedd472b65430a6f1949a3bff2864cdc443
Angelo Silva <angelosilva.goncalves@outlook.com.br>
2022-05-14 18:21:02 -0300

    Added index.js and App react component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/App';
+
+// CONFIG
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+// RENDER
+root.render(<App />)
